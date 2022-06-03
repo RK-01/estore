b12111262fe165fb57b1e1878dad5c40619cbd61
RK-01 <91008997+RK-01@users.noreply.github.com>
2022-06-03 20:27:32 +0530

    commit after adding products

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -10,7 +10,7 @@ const Product = ({product}) => {
     return (
         <>
         <Card className='my-3 rounded' style={{textAlign: "left"}}>
-            <Link to={`/product/${product._id}`}><Card.Img style={{margin: "0 auto", width: "150px", height:"auto"}} src={product.image} alt={product.name}/></Link>
+            <Link to={`/product/${product._id}`}><center><Card.Img style={{padding: "10px", width: "150px", height:"auto"}} src={product.image} alt={product.name}/></center></Link>
             <Card.Body>
                     <Card.Title as="div"><strong>{product.name}</strong></Card.Title>
             <Card.Text as="div"><Rating value={product.rating} text={`${product.numReviews} reviews`}/></Card.Text>
