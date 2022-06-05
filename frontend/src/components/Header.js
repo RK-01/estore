c9d3eddb8745e759a6f23353aee26a311c4190c9
RK-01 <91008997+RK-01@users.noreply.github.com>
2022-06-05 22:04:14 +0530

    cart display changed

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-import {Navbar, Nav, Container, NavDropdown,Button, Image, ListGroup} from 'react-bootstrap';
-import {logout} from '../actions/userActions'
+import { Navbar, Nav, Container, NavDropdown, Button, Image, ListGroup } from 'react-bootstrap';
+import { logout } from '../actions/userActions'
 import logo from '../logo.svg'
 import SearchBox from './SearchBox'
 import { PersonCircle, Cart, Gear, PersonPlus, CartCheck, BoxArrowRight, Folder, PersonLinesFill, BoxArrowInLeft } from "react-bootstrap-icons";
@@ -11,119 +11,122 @@ import { PersonCircle, Cart, Gear, PersonPlus, CartCheck, BoxArrowRight, Folder,
 
 
 const Header = () => {
+  const cartContent = useRef();
   const [displayCartSummary, setDisplayCartSummary] = useState(false)
   const [displayUserOptions, setDisplayUserOptions] = useState(false)
- 
-  const userLogin = useSelector(state=> state.userLogin);
- const {userInfo} = userLogin
- const dispatch = useDispatch();
+
+  const userLogin = useSelector(state => state.userLogin);
+  const { userInfo } = userLogin
+  const dispatch = useDispatch();
   const cart = useSelector(state => state.cart)
-  const {cartItems} = cart
+  const { cartItems } = cart
 
   const showCartSummary = (e) => {
     setDisplayCartSummary(true)
     setDisplayUserOptions(false)
   }
   const showUserOptions = (e) => {
-    
+
     setDisplayCartSummary(false)
     setDisplayUserOptions(true)
   }
-  
+
   const hideCartSummary = (e) => {
     setDisplayCartSummary(false)
   }
   const hideUserOptions = (e) => {
     setDisplayUserOptions(false)
   }
-  
+
   const logoutHandler = () => {
     dispatch(logout())
     setDisplayUserOptions(false)
   }
-  useEffect(()=>{
+  useEffect(() => {
     console.log(displayCartSummary)
-  },[dispatch, userInfo, displayCartSummary])
+  }, [dispatch, userInfo, displayCartSummary])
   return (
     <header>
       {/* New Header */}
       <div className="header-container">
-        
+
         <div className="rk-flex-row-container">
-        <div className="menu-container">
+          {/* <div className="menu-container">
             <i className='fas fa-bars'></i>
-        </div>
-        <div className="brand-container">
-          <Link to={`/`} ><Image src={logo} width='200px' fluid/></Link>
-        </div>
+          </div> */}
+          <div className="brand-container">
+            <Link to={`/`} ><Image src={logo} width='200px' fluid /></Link>
+          </div>
         </div>
         <div className="rk-flex-row-container">
           <div className="cart-container">
-          <Cart size={30} onClick={()=>showCartSummary()}/>
-          {cartItems.length > 0 ?
-          <div className="cart-count">{cartItems.reduce((acc, item)=> acc+Number(item.qty), 0)}</div>
-          :
-          <></>
-          } 
-          {displayCartSummary && 
-            (<div className="cart-content">
-            <div className="cart-content-header"><h3>Cart Total </h3><p>Rs. {cartItems.reduce((acc, item)=> acc + Number(item.qty) * item.price, 0)}</p></div>
-            {cartItems.map(item=>{
+            <Cart size={30} onClick={() => showCartSummary()} />
+            {cartItems.length > 0 ?
+              <div className="cart-count">{cartItems.reduce((acc, item) => acc + Number(item.qty), 0)}</div>
+              :
+              <></>
+            }
+          </div>
+          {displayCartSummary &&
+            (<div ref={cartContent} className="cart-content">
+              <div className="cart-content-header"><h3>Cart Summary </h3><button className="rk-btn-close" onClick={() => hideCartSummary()}>Close</button>
+              </div>
+              {cartItems.map(item => {
                 return (<div key={item.product} className="cart-summary">
-                    <div>
-                      <Image style={{width: "66px"}} src={item.image} alt={item.name} fluid rounded/>
-                    </div>
-                    <div>
-                      <small>{item.name}</small>
-                    </div>
-                    <div>
-                      <small>Rs. {item.price} x {item.qty}</small>
-                    </div>
+                  <div>
+                    <Image style={{ width: "66px" }} src={item.image} alt={item.name} fluid rounded />
+                  </div>
+                  <div>
+                    <small>{item.name}</small><br />
+                    <small style={{fontWeight:"bold"}}>Rs. {item.price} x {item.qty}</small>
+                  </div>
                 </div>
                 )
-            })
-            }
-            <div className="cart-summary-btns">
-              <button className="rk-btn-close" onClick={()=>hideCartSummary()}>Close</button>
-              <Link to={`/cart`} className="rk-btn"><span>Go to cart</span></Link>
+              })
+              }
+              <div className="cart-footer">
+                <Link to={`/cart`} className="go-to-cart-btn">
+                  Rs. {cartItems.reduce((acc, item) => acc + Number(item.qty) * item.price, 0)}<br />
+                  Go to cart
+                </Link>
+              </div>
             </div>
-            </div>
-)
+            )
           }
 
-                     
-            </div>
-          <div className="user-container"><PersonCircle onClick={()=>showUserOptions()} size={30} />
-             {userInfo ? (<>
-                          {displayUserOptions && 
-           <div className="user-content">
-             <div className="user-options-header"><PersonCircle size={25}/>&nbsp;{userInfo.name}</div>
-                <ListGroup>
-                  <Link to="/profile" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><PersonLinesFill size={15}/>&nbsp;Profile</ListGroup.Item></Link>
-                  <Link to="/myorders" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><CartCheck size={15}/>&nbsp;My Orders</ListGroup.Item></Link>
-                  <Link to="/settings" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><Gear size={15}/>&nbsp;Settings</ListGroup.Item></Link>
-                  {userInfo && userInfo.isAdmin && (
-                    <Link to="/admin-dashboard" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><Folder size={15}/>&nbsp;Admin</ListGroup.Item></Link>
-                  )}
-                  <ListGroup.Item onClick={logoutHandler} className="user-content-item"><BoxArrowRight size={15}/>&nbsp;Logout</ListGroup.Item>
-                  </ListGroup>
-                  <button className="rk-btn-close" style={{width:"100%"}} onClick={()=>hideUserOptions()}>Close</button>
-              </div>
-}
-             </>) : (
-               <>
-               {displayUserOptions && 
-                
+
+
+          <div className="user-container"><PersonCircle onClick={() => showUserOptions()} size={30} />
+            {userInfo ? (<>
+              {displayUserOptions &&
                 <div className="user-content">
+                  <div className="user-options-header"><PersonCircle size={25} />&nbsp;{userInfo.name}</div>
                   <ListGroup>
-                  <Link to="/login" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><BoxArrowInLeft />&nbsp;Sign In</ListGroup.Item></Link>
-                  <Link to="/register" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><PersonPlus />&nbsp;Sign Up</ListGroup.Item></Link>
+                    <Link to="/profile" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><PersonLinesFill size={15} />&nbsp;Profile</ListGroup.Item></Link>
+                    <Link to="/myorders" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><CartCheck size={15} />&nbsp;My Orders</ListGroup.Item></Link>
+                    <Link to="/settings" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><Gear size={15} />&nbsp;Settings</ListGroup.Item></Link>
+                    {userInfo && userInfo.isAdmin && (
+                      <Link to="/admin-dashboard" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><Folder size={15} />&nbsp;Admin</ListGroup.Item></Link>
+                    )}
+                    <ListGroup.Item onClick={logoutHandler} className="user-content-item"><BoxArrowRight size={15} />&nbsp;Logout</ListGroup.Item>
                   </ListGroup>
-                  <button className="rk-btn-close" style={{width:"100%"}} onClick={()=>hideUserOptions()}>Close</button>
+                  <button className="rk-btn-close" style={{ width: "100%" }} onClick={() => hideUserOptions()}>Close</button>
                 </div>
-}
-                </>
-                )}
+              }
+            </>) : (
+              <>
+                {displayUserOptions &&
+
+                  <div className="user-content">
+                    <ListGroup>
+                      <Link to="/login" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><BoxArrowInLeft />&nbsp;Sign In</ListGroup.Item></Link>
+                      <Link to="/register" style={{ textDecoration: "none" }}><ListGroup.Item className="user-content-item"><PersonPlus />&nbsp;Sign Up</ListGroup.Item></Link>
+                    </ListGroup>
+                    <button className="rk-btn-close" style={{ width: "100%" }} onClick={() => hideUserOptions()}>Close</button>
+                  </div>
+                }
+              </>
+            )}
 
           </div>
         </div>
@@ -185,10 +188,10 @@ const Header = () => {
     </div>
   </Container>
 </Navbar> */}
- <div style={{color:"#690fad", background: "#f6b93b", padding: "0px 20px 5px 20px"}}>
-  <SearchBox />
-    </div>
-    
+      <div style={{ color: "#690fad", background: "#f6b93b", padding: "0px 20px 5px 20px" }}>
+        <SearchBox />
+      </div>
+
     </header>
   )
 }
