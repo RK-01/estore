729fda980075e4235bf28385c12265f1991d4ccb
RK-01 <91008997+RK-01@users.noreply.github.com>
2022-06-05 13:49:04 +0530

    header changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,28 +1,136 @@
-import React, { useEffect } from 'react';
-import {LinkContainer} from 'react-router-bootstrap'
+import React, { useEffect, useState, useRef } from 'react';
+import {Link} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
-import {Navbar, Nav, Container, NavDropdown, Image} from 'react-bootstrap';
+import {Navbar, Nav, Container, NavDropdown,Button, Image, ListGroup} from 'react-bootstrap';
 import {logout} from '../actions/userActions'
 import logo from '../logo.svg'
 import SearchBox from './SearchBox'
+import { PersonCircle, Cart, Gear, PersonPlus, CartCheck, BoxArrowRight, Folder, PersonLinesFill, BoxArrowInLeft } from "react-bootstrap-icons";
+
+
 
 
 const Header = () => {
- const userLogin = useSelector(state=> state.userLogin);
+  const [displayCartSummary, setDisplayCartSummary] = useState(false)
+  const [displayUserOptions, setDisplayUserOptions] = useState(false)
+ 
+  const userLogin = useSelector(state=> state.userLogin);
  const {userInfo} = userLogin
  const dispatch = useDispatch();
   const cart = useSelector(state => state.cart)
   const {cartItems} = cart
 
+  const showCartSummary = (e) => {
+    setDisplayCartSummary(true)
+    setDisplayUserOptions(false)
+  }
+  const showUserOptions = (e) => {
+    
+    setDisplayCartSummary(false)
+    setDisplayUserOptions(true)
+  }
+  
+  const hideCartSummary = (e) => {
+    setDisplayCartSummary(false)
+  }
+  const hideUserOptions = (e) => {
+    setDisplayUserOptions(false)
+  }
+  
   const logoutHandler = () => {
     dispatch(logout())
+    setDisplayUserOptions(false)
   }
   useEffect(()=>{
-
-  },[dispatch, userInfo])
+    console.log(displayCartSummary)
+  },[dispatch, userInfo, displayCartSummary])
   return (
     <header>
-      <Navbar className="sticky" expand="lg" style={{color:"#690fad", background: "#f6b93b", padding: "0px"}} collapseOnSelect>
+      {/* New Header */}
+      <div className="header-container">
+        
+        <div className="rk-flex-row-container">
+        <div className="menu-container">
+            <i className='fas fa-bars'></i>
+        </div>
+        <div className="brand-container">
+          <Link to={`/`} ><Image src={logo} width='200px' fluid/></Link>
+        </div>
+        </div>
+        <div className="rk-flex-row-container">
+          <div className="cart-container">
+          <Cart size={30} onClick={()=>showCartSummary()}/>
+          {cartItems.length > 0 ?
+          <div className="cart-count">{cartItems.reduce((acc, item)=> acc+Number(item.qty), 0)}</div>
+          :
+          <></>
+          } 
+          {displayCartSummary && 
+            (<div className="cart-content">
+            <div className="cart-content-header"><h3>Cart Total </h3><p>Rs. {cartItems.reduce((acc, item)=> acc + Number(item.qty) * item.price, 0)}</p></div>
+            {cartItems.map(item=>{
+                return (<div key={item.product} className="cart-summary">
+                    <div>
+                      <Image style={{width: "66px"}} src={item.image} alt={item.name} fluid rounded/>
+                    </div>
+                    <div>
+                      <small>{item.name}</small>
+                    </div>
+                    <div>
+                      <small>Rs. {item.price} x {item.qty}</small>
+                    </div>
+                </div>
+                )
+            })
+            }
+            <div className="cart-summary-btns">
+              <button className="rk-btn-close" onClick={()=>hideCartSummary()}>Close</button>
+              <Link to={`/cart`} className="rk-btn"><span>Go to cart</span></Link>
+            </div>
+            </div>
+)
+          }
+
+                     
+            </div>
+          <div className="user-container"><PersonCircle onClick={()=>showUserOptions()} size={30} />
+             {userInfo ? (<>
+                          {displayUserOptions && 
+           <div className="user-content">
+             <div className="user-options-header"><PersonCircle size={25}/>&nbsp;{userInfo.name}</div>
+                <ListGroup>
+                  <Link to="/profile" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><PersonLinesFill size={15}/>&nbsp;Profile</ListGroup.Item></Link>
+                  <Link to="/myorders" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><CartCheck size={15}/>&nbsp;My Orders</ListGroup.Item></Link>
+                  <Link to="/settings" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><Gear size={15}/>&nbsp;Settings</ListGroup.Item></Link>
+                  {userInfo && userInfo.isAdmin && (
+                    <Link to="/admin-dashboard" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><Folder size={15}/>&nbsp;Admin</ListGroup.Item></Link>
+                  )}
+                  <ListGroup.Item onClick={logoutHandler} className="user-content-item"><BoxArrowRight size={15}/>&nbsp;Logout</ListGroup.Item>
+                  </ListGroup>
+                  <button className="rk-btn-close" style={{width:"100%"}} onClick={()=>hideUserOptions()}>Close</button>
+              </div>
+}
+             </>) : (
+               <>
+               {displayUserOptions && 
+                
+                <div className="user-content">
+                  <ListGroup>
+                  <Link to="/login" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><BoxArrowInLeft />&nbsp;Sign In</ListGroup.Item></Link>
+                  <Link to="/register" style={{textDecoration: "none"}}><ListGroup.Item className="user-content-item"><PersonPlus />&nbsp;Sign Up</ListGroup.Item></Link>
+                  </ListGroup>
+                  <button className="rk-btn-close" style={{width:"100%"}} onClick={()=>hideUserOptions()}>Close</button>
+                </div>
+}
+                </>
+                )}
+
+          </div>
+        </div>
+      </div>
+
+      {/* Old header */}
+      {/* <Navbar className="sticky" expand="lg" style={{color:"#690fad", background: "#f6b93b", padding: "0px"}} collapseOnSelect>
   <Container style={{display:"flex", justifyContent: "space-between"}} fluid>
     <div>
     <Navbar.Brand href="/"><Image src={logo} width='200px' fluid/></Navbar.Brand>
@@ -76,7 +184,7 @@ const Header = () => {
     </Navbar.Collapse>
     </div>
   </Container>
-</Navbar>
+</Navbar> */}
  <div style={{color:"#690fad", background: "#f6b93b", padding: "0px 20px 5px 20px"}}>
   <SearchBox />
     </div>
