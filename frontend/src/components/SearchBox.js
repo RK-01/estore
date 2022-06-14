779f3a13112ad66f630341da81694977bb922230
RK-01 <91008997+RK-01@users.noreply.github.com>
2022-06-14 22:10:00 +0530

    search bar changes

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBorderNone, faMagnifyingGlass, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const SearchBox = () => {
     const [keyword, setKeyword] = useState('')
@@ -16,22 +18,18 @@ const SearchBox = () => {
         
     }
     return (
-    <Form style={{display:'flex'}} onSubmit={searchFormSubmit}>
-        <Form.Control
-            type="text"
+        <Form onSubmit={searchFormSubmit}>
+    <div className="search-bar-wrapper">
+        <div className="fas fa-search search-icon"></div>   
+            <input
+            type="search"
             name="q"
             onChange={(e)=>setKeyword(e.target.value)} 
             placeholder="Search..."
-            className="mr-sm-2 ml-sm-5"
-            style={{borderRadius: "25px 0px 0px 25px"}}
-            ></Form.Control>
-            <Button 
-                type="submit" 
-                variant="outline-success" 
-                className="p-2"
-                style={{borderRadius: "0px 25px 25px 0px", background: "#690fad", color: "#fff", border: "1px solid #690fad", width: "70px"}}
-                ><i className="fas fa-search"></i></Button>
-        
+            style={{background: "#fff", alignSelf: 'stretch',height: "100%", border: 'none', outline: 'none'}}
+            />
+            <div className=""><button className="search-btn" type="submit">Search</button></div>
+    </div>
     </Form>
   )
 }
